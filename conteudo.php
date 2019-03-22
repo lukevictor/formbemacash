9d0d3285b76ac9d019a2ffd554114864cbff3a69
Lucas <lucas.c.victor@hotmail.com>
2019-03-22 19:15:04 -0300

    Correções de concatenação do conteudo

--- conteudo.php
+++ conteudo.php
@@ -4,20 +4,20 @@
 function montaConteudo($nome, $telefone, $email, $cnpj) {
 
     $respostaHTML  = "<p> Informações enviadas pelo formulário de contato do FIRST (Série 1) </p>";
-    $respostaHTML += "<p>Este cliente está interessado em saber mais sobre o Bemacash Gestão</p>";
+    $respostaHTML = $respostaHTML . "<p>Este cliente está interessado em saber mais sobre o Bemacash Gestão</p>";
 
-    $respostaHTML += '<h3> Informações repassadas pelo cliente: </h3>';
-    $respostaHTML += '<p> Nome: ' .     $nome .     '</p>';
-    $respostaHTML += '<p> Telefone: ' . $telefone . '</p>';
-    $respostaHTML += '<p> Email: ' .    $email .    '</p>';
-    $respostaHTML += '<p> CNPJ: ' .     $cnpj .     '</p>';
+    $respostaHTML = $respostaHTML . '<h3> Informações repassadas pelo cliente: </h3>';
+    $respostaHTML = $respostaHTML . '<p> Nome: ' .     $nome .     '</p>';
+    $respostaHTML = $respostaHTML . '<p> Telefone: ' . $telefone . '</p>';
+    $respostaHTML = $respostaHTML . '<p> Email: ' .    $email .    '</p>';
+    $respostaHTML = $respostaHTML . '<p> CNPJ: ' .     $cnpj .     '</p>';
 
 
-    $respostaHTML += '<h3> Informações contidas no sistema (FIRST) </h3>';
-    $respostaHTML += '<p> Empresa: ' .            $sNome .     '</p>';
-    $respostaHTML += '<p> Telefone Comercial: ' . $sTelefone . '</p>';
-    $respostaHTML += '<p> Email Comercial: ' .    $sEmail .    '</p>';
-    $respostaHTML += '<p> CNPJ: ' .               $sCnpj .     '</p>';
+    $respostaHTML = $respostaHTML . '<h3> Informações contidas no sistema (FIRST) </h3>';
+    $respostaHTML = $respostaHTML . '<p> Empresa: ' .            $sNome .     '</p>';
+    $respostaHTML = $respostaHTML . '<p> Telefone Comercial: ' . $sTelefone . '</p>';
+    $respostaHTML = $respostaHTML . '<p> Email Comercial: ' .    $sEmail .    '</p>';
+    $respostaHTML = $respostaHTML . '<p> CNPJ: ' .               $sCnpj .     '</p>';
 
     return $respostaHTML;
 }
--- receptor.php
+++ receptor.php
@@ -1,16 +1,17 @@
 <?php 
 
-$nome = $_GET['nome'];
-$fone = $_GET['fone'];
+//Informações repassadas pelo cliente
+$nome  = $_GET['nome'];
+$fone  = $_GET['fone'];
 $email = $_GET['email'];
-$cnpj = $_GET['cnpj'];
+$cnpj  = $_GET['cnpj'];
 
 //Informações do sistema recebidas de forma oculta
-$sBairro = $_GET['sBairro'];
-$sCidade = $_GET['sCidade'];
-$sEstado = $_GET['sEstado'];
-$sCEP = $_GET['sCep'];
+$sBairro   = $_GET['sBairro'];
+$sCidade   = $_GET['sCidade'];
+$sEstado   = $_GET['sEstado'];
+$sCEP      = $_GET['sCep'];
 $sTelefone = $_GET['sFone'];
-$sEmail = $_GET['sEmail'];
+$sEmail    = $_GET['sEmail'];
 
 ?>

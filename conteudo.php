dc2dabfc9df1a44255b2efe522123e34bf609a63
stark <lucas.c.victor@gmail.com>
2019-03-23 16:20:41 -0300

    Correções de encoding e recebimento de informações

--- conteudo.php
+++ conteudo.php
@@ -2,17 +2,17 @@
 
 function montaConteudo($nome, $telefone, $email, $cnpj, $sNome, $sTelefone, $sEmail, $sCnpj, $sCep, $sCidade, $sEstado, $sBairro, $sEndereco) {
 
-    $respostaHTML  = "<p> Informações enviadas pelo formulário de contato do FIRST (Série 1) </p>";
-    $respostaHTML = $respostaHTML . "<p>Este cliente está interessado em saber mais sobre o Bemacash Gestão</p>";
+    $respostaHTML  = "<p> InformaÃ§Ãµes enviadas pelo formulÃ¡rio de contato do FIRST (SÃ©rie 1) </p>";
+    $respostaHTML = $respostaHTML . "<p>Este cliente estÃ¡ interessado em saber mais sobre o Bemacash GestÃ£o</p>";
 
-    $respostaHTML = $respostaHTML . '<h3> Informações repassadas pelo cliente: </h3>';
+    $respostaHTML = $respostaHTML . '<h3> InformaÃ§Ãµes repassadas pelo cliente: </h3>';
     $respostaHTML = $respostaHTML . '<p> Nome: ' .     $nome .     '</p>';
     $respostaHTML = $respostaHTML . '<p> Telefone: ' . $telefone . '</p>';
     $respostaHTML = $respostaHTML . '<p> Email: ' .    $email .    '</p>';
     $respostaHTML = $respostaHTML . '<p> CNPJ: ' .     $cnpj .     '</p>';
 
 
-    $respostaHTML = $respostaHTML . '<h3> Informações contidas no sistema (FIRST) </h3>';
+    $respostaHTML = $respostaHTML . '<h3> InformaÃ§Ãµes contidas no sistema (FIRST) </h3>';
     $respostaHTML = $respostaHTML . '<p> Empresa: ' .            $sNome .     '</p>';
     $respostaHTML = $respostaHTML . '<p> Telefone Comercial: ' . $sTelefone . '</p>';
     $respostaHTML = $respostaHTML . '<p> Email Comercial: ' .    $sEmail .    '</p>';

f2b5949d9f1d80214208b11edad0e479a83aaac7
Lucas <lucas.c.victor@hotmail.com>
2019-03-25 08:51:46 -0300

    Correções de posicionamento de campos

--- index.php
+++ index.php
@@ -14,38 +14,46 @@
 </head>
 <body>
 <div style="margin:20px;">
-<h1 >Complemente seus dados abaixo:</h1>
-<h2 style="color: orange"> Equipe Bemacash Gestão </h2>
-<form action="email.php" method="GET" id="contatoBemacash">
-    <p >
-	<label for="Nome">Nome:</label>
-	<input type="text" name="nome" id="nome" size="35" class="campo" value=<?=$nome ?>>
-    </p>
-    <p >
-	<label for="Email">E-mail:</label>
-	<input type="text" name="email" id="email" size="35" class="campo" value=<?=$email ?>>
-    </p>
-    <p >
-	<label for="Fone">Telefone:</label>
-	<input type="text" name="fone" id ="fone" size="35" class="campo" value=<?=$fone ?>>
-    </p>
-    <p >
-	<label for="CNPJ">CNPJ/CPF:</label>
-	<input type="text" name="cnpj" id="cnpj" size="35" class="campo"value=<?=$cnpj ?>>
-    </p>
-    <p >
-    <input type="hidden" name="sCnpj"     value=<?=$sCnpj ?>>
-    <input type="hidden" name="sCep"      value=<?=$sCep ?>>
-    <input type="hidden" name="sCidade"   value=<?=$sCidade ?>>
-    <input type="hidden" name="sBairro"   value=<?=$sBairro ?>>
-    <input type="hidden" name="sEstado"   value=<?=$sEstado ?>>
-    <input type="hidden" name="sEndereco" value=<?=$sEndereco ?>>
-    <input type="hidden" name="sFone"     value=<?=$sFone ?>>
-    <input type="hidden" name="sNome"     value=<?=$sNome ?>>
-    <input type="hidden" name="sEmail"    value=<?=$sEmail ?>>
-	<input type="button" onClick="checkForm();" value="Enviar">
-    </p>
-</form>
+
+<div class="row">
+    <div class="col s9">
+        <h3 >Complemente seus dados abaixo:</h1>
+        <h4 style="color: orange"> Equipe Bemacash Gestão </h2>
+    </div>
+    <div class="col s6">
+        <form action="email.php" method="GET" id="contatoBemacash">
+            <p >
+            <label for="Nome">Nome:</label>
+            <input type="text" name="nome" id="nome" size="35" class="campo" value=<?=$nome ?>>
+            </p>
+            <p >
+            <label for="Email">E-mail:</label>
+            <input type="text" name="email" id="email" size="35" class="campo" value=<?=$email ?>>
+            </p>
+            <p >
+            <label for="Fone">Telefone:</label>
+            <input type="text" name="fone" id ="fone" size="35" class="campo" value=<?=$fone ?>>
+            </p>
+            <p >
+            <label for="CNPJ">CNPJ/CPF:</label>
+            <input type="text" name="cnpj" id="cnpj" size="35" class="campo"value=<?=$cnpj ?>>
+            </p>
+            <p >
+            <input type="hidden" name="sCnpj"     value=<?=$sCnpj ?>>
+            <input type="hidden" name="sCep"      value=<?=$sCep ?>>
+            <input type="hidden" name="sCidade"   value=<?=$sCidade ?>>
+            <input type="hidden" name="sBairro"   value=<?=$sBairro ?>>
+            <input type="hidden" name="sEstado"   value=<?=$sEstado ?>>
+            <input type="hidden" name="sEndereco" value=<?=$sEndereco ?>>
+            <input type="hidden" name="sFone"     value=<?=$sFone ?>>
+            <input type="hidden" name="sNome"     value=<?=$sNome ?>>
+            <input type="hidden" name="sEmail"    value=<?=$sEmail ?>>
+            <input type="button" onClick="checkForm();" value="Enviar">
+            </p>
+        </form>
+    </div>
+</div>
+
 
 TOTVS 2019
 </div>

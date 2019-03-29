e3a289053c69db958db28ed9a505a01dfb9ad425
Lucas <lucas.c.victor@hotmail.com>
2019-03-29 18:55:46 -0300

    Remoção da validação de campos

--- js/validador.js
+++ js/validador.js
@@ -1,5 +1,5 @@
 function checkForm() {
-    var nome = document.getElementById("nome").value;
+/*    var nome = document.getElementById("nome").value;
     var email = document.getElementById("email").value;
     var fone = document.getElementById("fone").value;
     var cnpj = document.getElementById("cnpj").value;
@@ -7,8 +7,8 @@ function checkForm() {
     if (nome == '' || email == '' || fone == '' || cnpj == '') {
         alert("Por favor preencha todos os campos.");
         return false;
-    } else {
+    } else { */
         document.getElementById("contatoBemacash").submit();
         return true;
-    }
+    //}
 }

36a272d24603db81b4a03aec6e72c21183540578
Lucas <lucas.c.victor@hotmail.com>
2019-04-04 14:07:13 -0300

    'Emulação' da página do bemacash

---
+++ form.php
@@ -0,0 +1,55 @@
+<div class="modal-body">
+    <div class="modal-form">
+     <div role="form" class="wpcf7" id="wpcf7-f781-o1" lang="pt-BR" dir="ltr">
+      <div class="screen-reader-response"></div>
+      <form action="/gestao-financeira#wpcf7-f781-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+      <div style="display: none;"> 
+        <input type="hidden" name="_wpcf7" value="781" /> 
+        <input type="hidden" name="_wpcf7_version" value="5.0.5" /> 
+        <input type="hidden" name="_wpcf7_locale" value="pt_BR" /> 
+        <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f781-o1" /> 
+        <input type="hidden" name="_wpcf7_container_post" value="0" />
+        <input type="hidden" name="sCnpj"     value=<?=$sCnpj ?>>
+        <input type="hidden" name="sCep"      value=<?=$sCep ?>>
+        <input type="hidden" name="sCidade"   value=<?=$sCidade ?>>
+        <input type="hidden" name="sBairro"   value=<?=$sBairro ?>>
+        <input type="hidden" name="sEstado"   value=<?=$sEstado ?>>
+        <input type="hidden" name="sEndereco" value=<?=$sEndereco ?>>
+        <input type="hidden" name="sFone"     value=<?=$sFone ?>>
+        <input type="hidden" name="sNome"     value=<?=$sNome ?>>
+        <input type="hidden" name="sEmail"    value=<?=$sEmail ?>>
+      </div>
+      <h3 class="form-title" style="font-size: 34px"> Dê mais este passo para o crescimento do <br/> seu negócio, preencha abaixo ou ligue para 0800 718 6887.<br /></h3>
+      <div class="form-row">
+      <div class="form-group col-md-12"> 
+      <label for="name"><br /> Nome completo <span> (obrigatório) </span><br /> </label><br /> 
+      <span class="wpcf7-form-control-wrap your-name">
+      <input type="text" name="nome" value="<?=$nome ?>" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" id="name" aria-required="true" aria-invalid="false" placeholder="Nome completo" /></span>
+      </div>
+      <div class="form-group col-md-12"> 
+      <label for="email"><br /> E-mail para contato <span> (obrigatório) </span><br /> </label>
+      <br /> 
+      <span class="wpcf7-form-control-wrap your-email">
+      <input type="email" name="email" value="<?=$email ?>" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control" id="email" aria-required="true" aria-invalid="false" placeholder="E-mail" />
+      </span>
+      </div>
+      <div class="form-group col-md-12"> 
+      <label for="phone">
+      <br /> Telefone <span> (obrigatório) </span>
+      <br /> </label>
+      <br /> 
+      <span class="wpcf7-form-control-wrap your-phone">
+      <input type="text" name="fone" value="<?=$fone ?>" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" id="phone" aria-required="true" aria-invalid="false" placeholder="Telefone" /></span>
+      </div>
+      <p> 
+      <input type="submit" value="Receba nossa ligação" class="wpcf7-form-control wpcf7-submit button __big" /></p>
+      </div>
+      <div class="wpcf7-response-output wpcf7-display-none">
+      </div>
+      </form>
+      </div> <!--<h3 id="hs_form_title" class="form-title">D� mais este passo para o crescimento do <br/> seu neg�cio, preenchendo abaixo:</h3> -->
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>

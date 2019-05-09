2947d213686110885a4e247bb49298d76fbe1d1f
Lucas <lucas.c.victor@hotmail.com>
2019-05-09 09:34:41 -0300

    Correções para permissão de CORS

--- email.php
+++ email.php
@@ -6,6 +6,9 @@ require 'receptor.php';
 require 'vendor/autoload.php';
 require 'conteudo.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 //Create a new PHPMailer instance
 $mail = new PHPMailer;

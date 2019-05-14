a7336d0664bd0c7ebb43ef0c8e0c5a39671dcdb0
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2019-05-14 14:07:37 -0300

    Correções para recebimento de informações para o tipo de log

--- slack.php
+++ slack.php
@@ -6,7 +6,15 @@ $json = file_get_contents('php://input');
 
 //$json = json_encode($array);
 
-$ch = curl_init('https://hooks.slack.com/services/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');
+if (strpos(json_decode($json), '"tipo":1')) {
+    $ch = curl_init('https://hooks.slack.com/services/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');
+} else {
+    $ch = curl_init('https://URL SERÁ Criada');
+}
+
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => 1
+]);
 
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 

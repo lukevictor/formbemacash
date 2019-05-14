9b1318ebd3cf4200822ff49c654f19e11d4eb401
Lucas da Cunha Victor <lucas.victor@totvs.com.br>
2019-05-14 10:26:45 -0300

    Criação de teste do slack

---
+++ slack.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+$array = array("text" => "Teste Slack");
+
+$json = json_encode($array);
+
+$ch = curl_init('https://hooks.slack.com/services/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');
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+
+    'Content-Type: application/json',
+
+    'Content-Length: ' . strlen($json))
+
+);
+
+$jsonRet = json_decode(curl_exec($ch));
+
+
+echo('Teste muito louco:' . $jsonRet);
+
+?>
